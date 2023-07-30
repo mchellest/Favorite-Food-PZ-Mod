346f0baebcd53e2c7cf2528d618cc5fbb6660e19
Michelle Stecko <michellesteckoweb@gmail.com>
2023-07-30 11:02:34 -0500

    Add custom traits file

---
+++ media/lua/shared/NPCs/FFTraits.lua
@@ -0,0 +1,11 @@
+FFTraitsDetails = {};
+-- addTrait(string: Name, string: Name, int: Cost, string: Tooltip text, boolean: isFree)
+-- Traits that have isFree as true will not show the tooltip text on hover
+-- Traits that have isFree as false will be sorted into Good/Bad traits and show tooltip on hover
+FFTraitsDetails.DoTraits = function ()
+    local favFoodChicken = TraitFactory.addTrait("FavFoodChicken", "Chicken", 0, "This increases positive buffs your character gets from eating Chicken", false);
+    local favFoodBacon = TraitFactory.addTrait("FavFoodBacon", "Bacon", 0, "This increases positive buffs your character gets from eating Bacon", false);
+
+end
+
+Events.OnGameBoot.Add(FFTraitsDetails.DoTraits);
